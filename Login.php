66fd3b61626fe40c40008fdaf9f4842cc9ac6d32
lp0525 <106250022+lp0525@users.noreply.github.com>
2022-09-15 04:10:23 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -11,7 +11,7 @@
 	$clave = $_POST["txtClave"];
 	
 	// Crear La Conexion a MySQL
-	$conexion = mysqli_connect("localhost", "root", "");
+	$conexion = mysqli_connect("localhost", "root", "root");
 	
 	// Seleccionar La Base De Datos
 	mysqli_select_db($conexion, "alumnosDB");
